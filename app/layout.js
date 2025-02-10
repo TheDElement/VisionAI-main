f6a9e69209410be6cd105fefdc822623e227bcbc
TheDElement <195310693006.mca19@scet.ac.in>
2025-02-10 00:11:57 -0500

    Error in Middle Ware

--- app/layout.js
+++ app/layout.js
@@ -14,11 +14,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider 
-    appearance={{
-      baseTheme:dark,
-    }}
-    >
+    // <ClerkProvider 
+    // appearance={{
+    //   baseTheme:dark,
+    // }}
+    // >
     
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className}`}>
@@ -44,6 +44,6 @@ export default function RootLayout({ children }) {
         </ThemeProvider>
       </body>
     </html>
-    </ClerkProvider>
+    
   );
 }
