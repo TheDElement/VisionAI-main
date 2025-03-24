47c11722823eaf986495254338687c19a15efc3a
Hasti0111 <hasti.ak28@gmail.com>
2025-03-23 22:42:46 -0400

    Cover letter get From API and Database - Hasti

--- actions/cover-letter.js
+++ actions/cover-letter.js
@@ -64,3 +64,41 @@ export async function generateCoverLetter(data) {
     throw new Error("Failed to generate cover letter");
   }
 }
+
+export async function getCoverLetters() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  return await db.coverLetter.findMany({
+    where: {
+      userId: user.id,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+}
+
+export async function getCoverLetter(id) {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) throw new Error("User not found");
+
+  return await db.coverLetter.findUnique({
+    where: {
+      id,
+      userId: user.id,
+    },
+  });
+}
